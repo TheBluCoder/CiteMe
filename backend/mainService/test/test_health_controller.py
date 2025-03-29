8c8ae4e64bb4f002aac5116f927a646cc0df069f
Ikeoluwa Oladele <oladelesamson156@gmail.com>
2025-03-29 16:14:04 -0400

    tracking frontend only

--- backend/mainService/test/test_health_controller.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-
-def test_health_check(test_client):
-    response = test_client.get("/health")
-    
-    assert response.status_code == 200
-    assert response.json() == {"status": "Healthy"} 
